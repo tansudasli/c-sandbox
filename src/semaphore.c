4bad218c231ef9e2c154b3251e2e2b5591afa8db
tansudasli <tansudasli@gmail.com>
2021-09-17 14:40:02 +0300

    bugfix: sem_init replaced w/ sem_open

--- src/semaphore.c
+++ src/semaphore.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-sem_t s;
+#define BUFFER_SIZE 10
+
+sem_t *s;
 unsigned int X = 0;   //initial semaphore value
 
 void * f (void *arg) {
 
     printf("child started...\n");
-    sem_post(&s);        //signal that child is done!
+    sem_post(s);        //signal that child is done!
 
     return NULL;
 }
@@ -16,13 +19,18 @@ void * f (void *arg) {
 //gcc -o out/semaphore src/semaphore.c  && out/semaphore
 int main(int argc, char **argv) {
 
-    sem_init(&s, 0, X);
+    s = sem_open("/dummy", O_CREAT, 0644, 0);
+
+    if (s == SEM_FAILED) {
+        perror("semaphore creation error\n");
+        exit(-1);
+    }
 
     printf("parent started...\n");
 
     pthread_t c;
     pthread_create(&c, NULL, f, NULL);
-    sem_wait(&s);         //wait the child
+    sem_wait(s);         //wait the child
 
     printf("parent end...\n");
     return 0;
