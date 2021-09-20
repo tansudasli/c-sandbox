09cb2d2a4ef252d08e29b9609d3463e6a67fca76
tansudasli <tansudasli@gmail.com>
2021-09-20 13:16:50 +0300

    semaphore as mutex

--- src/threading/semaphore.c
+++ src/threading/semaphore.c
@@ -5,21 +5,24 @@
 
 #define BINARY 1        //binary semaphore works as mutex
 
-sem_t *s;
+sem_t *mutex;
 
 void *f (void *arg) {
 
     printf("child started...\n");
-    sem_post(s);        //signal that child is done!
+
+    //critical code
+    sem_post(mutex);        //signal that child is done!
 
     return NULL;
 }
 
 //gcc -o out/semaphore src/semaphore.c  && out/semaphore
+//parent works only child ends
 int main(int argc, char **argv) {
 
-    s = sem_open("/dummy", O_CREAT, 0644, BINARY);
-    if (s == SEM_FAILED) {
+    mutex = sem_open("/dummy", O_CREAT, 0644, BINARY);
+    if (mutex == SEM_FAILED) {
         perror("semaphore creation error\n");
         exit(-1);
     }
@@ -28,8 +31,10 @@ int main(int argc, char **argv) {
 
     pthread_t c;
     pthread_create(&c, NULL, f, NULL);
-    sem_wait(s);         //wait the child
+    sem_wait(mutex);         //wait here for the child
 
+    //do some parent staff
     printf("parent end...\n");
+
     return 0;
 }
