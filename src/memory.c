e6dfedc3d032e42d9a6ec8ba63541609e484e4a8
tansudasli <tansudasli@gmail.com>
2021-09-22 12:50:12 +0300

    restructuring

--- src/memory.c
+++ src/memory.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_LENGTH 3
+const static int y = 4;
 
 int main (int argc, char **argv) {
-    int i;
-    int n = !argv[1] ?  MAX_LENGTH : atoi(argv[1]);
-    int *a = (int*) calloc(n, sizeof(int));  //to initialize a pointer, use either &int[], or malloc/calloc !!
-                                                  //count is not important in IDE (somehow) but It is critical on shell :)
-                                                  //initialize w/ 0
 
-    printf("Enter %d numbers:\n",n);
-    for( i=0 ; i < n ; i++ ) {
-        scanf("%d", &a[i]);
-    }
+    int x = 3;
 
-    printf("The numbers entered are: ");
-    for( i=0 ; i < n ; i++ ) {
-        printf("%d ", a[i]);
-    }
-    free( a );
+    //all virtual memory address spaces!!
+    printf("location of ... \n");
+    printf("code= %p \theap=%p \tstack=%p \tglobal=%p\n", main,
+                                              malloc(10e2),
+                                              &x,
+                                              &y);
 
-    return(0);
+
+    return EXIT_SUCCESS;
 }
--- src/memory2.c
+++ src/memory2.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const static int y = 4;
+#define MAX_LENGTH 3
 
 int main (int argc, char **argv) {
+    int i;
+    int n = !argv[1] ?  MAX_LENGTH : atoi(argv[1]);
+    int *a = (int*) calloc(n, sizeof(int));  //to initialize a pointer, use either &int[], or malloc/calloc !!
+                                                  //count is not important in IDE (somehow) but It is critical on shell :)
+                                                  //initialize w/ 0
 
-    int x = 3;
+    printf("Enter %d numbers:\n",n);
+    for( i=0 ; i < n ; i++ ) {
+        scanf("%d", &a[i]);
+    }
 
-    //all virtual memory address spaces!!
-    printf("location of ... \n");
-    printf("code= %p \theap=%p \tstack=%p \tglobal=%p\n", main,
-                                              malloc(10e2),
-                                              &x,
-                                              &y);
+    printf("The numbers entered are: ");
+    for( i=0 ; i < n ; i++ ) {
+        printf("%d ", a[i]);
+    }
+    free( a );
 
-
-    return EXIT_SUCCESS;
+    return(0);
 }
