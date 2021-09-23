c89000173d2c8338945774f5609cc7e9dc762a0e
tansudasli <tansudasli@gmail.com>
2021-09-23 12:53:05 +0300

    simple threading

--- src/concurrency/threading.c
+++ src/concurrency/threading.c
@@ -4,9 +4,45 @@
 #include <pthread.h>
 #include "common_threads.h"
 
+void *xyz(void *arg) {
+    printf("thread=%s\n", (char *)arg);
+
+    return NULL;
+}
+
+/**
+ * There are 3 critical concepts in threading.
+ *    + (1)Reframe the problem to make concurrent and parallel
+ *    + (2)Define critical parts
+ *    + (3)Define how to communicate each other
+ *
+ *
+ * 2- sync
+ *   - locking (mutex)
+ *   - signalling (CV)
+ * 3- communication
+ *   - message passing (sockets, pipes, mq, file ..)
+ *   - shared memory
+ *
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main (int argc, char **argv) {
+    pthread_t t1, t2;
+    int rc;
 
+    printf("parent start...\n");
 
+    Pthread_create(&t1, NULL, xyz, "t1");
+    Pthread_create(&t2, NULL, xyz, "t2");
+
+    //parent has to wait threads execution.
+    Pthread_join(t1, NULL);
+    Pthread_join(t2, NULL);
+
+    printf("parent end...\n");
 
     return EXIT_SUCCESS;
 }
