e682f87423f61936629a53875e8b1823edbc73b1
tansudasli <tansudasli@gmail.com>
2022-12-10 15:04:01 +0300

    new explanations added

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
 #on linux/Mac/windows
-#under ansi-c folder
+#under c-sandbox folder
 cmake . -B out   #creates cmake caches
 make -C out      #builds
 
 #on IDE - clion
-#cmake --build cmake-build-debug --target all -- -j 30
+#load CMakeLists.txt file in IDE. So that you can run manually.
+#If error occurs, delete cmake-build-debug folder...
+#cmake --build cmake-build-debug --target all -- -j 30  ????not working some parameters anymore.
